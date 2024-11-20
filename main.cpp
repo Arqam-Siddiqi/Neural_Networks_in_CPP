abfe25576c694850eb42b9582211f64a38d8ad28
Arqam-Siddiqi <siddiqi.seerow@gmail.com>
2024-11-20 23:39:17 +0500

    Working Linear Regression model.

--- main.cpp
+++ main.cpp
@@ -7,93 +7,6 @@
 
 using namespace std;
 
-class LinearRegression {
-
-    private:
-        double* weights;
-        double bias;
-        int n;
-
-    public:
-
-        LinearRegression(int n){
-            this->n = n;
-            
-            weights = new double[n];
-            for(int i=0; i<n; i++){
-                weights[i] = 0;
-            }
-
-            bias = 0;
-        }
-
-        double f(double x){
-            return weights[0] * x + bias;
-        }
-
-        double* fit(double** data, int length, double learning_rate, int epochs){
-
-            double* cost_history = new double[epochs+1];
-
-            cost_history[0] = compute_cost(data, length);
-
-            # pragma omp parallel for
-            for(int i = 0; i<epochs; i++){
-                double* gradients = compute_gradients(data, length);
-
-                double temp_w[n];
-                for(int i=0; i<n; i++){
-                    temp_w[i] = weights[i] - learning_rate * gradients[0];
-                }
-                double temp_b = bias - learning_rate * gradients[1];
-
-                delete gradients;
-
-                for(int i=0; i<n; i++){
-                    weights[i] = temp_w[i];
-                }
-                bias = temp_b;
-
-                cost_history[i+1] = compute_cost(data, length);
-            }
-
-            return cost_history;
-        }
-
-        double* compute_gradients(double** data, int length){
-
-            double* gradients = new double[2];
-
-            for(int i = 0; i<length; i++){
-                double x = data[i][0], y = data[i][1];
-                gradients[0] = (f(x) - y) * x;
-                gradients[1] = (f(x) - y);
-            }
-
-            gradients[0] /= length;
-            gradients[1] /= length;
-
-            return gradients;
-        }
-
-        double compute_cost(double** data, int length){
-
-            double loss = 0;
-
-            for(int i = 0; i<length; i++){
-                loss = pow(f(data[i][0]) - data[i][1], 2);
-            }
-            loss /= length;
-
-            return loss;
-        }
-
-        double predict(double x){
-            return weights[0] * x + bias;
-        }
-
-};
-
 class Reader {
 
     private:
@@ -121,8 +34,6 @@ class Reader {
             
             file.close();
 
-            
-
             int* size = new int[2];
             size[0] = rows;
             size[1] = cols;
@@ -131,82 +42,189 @@ class Reader {
         }
 
     public:
-        static pair<int, double**> readCSV(string path){
+        static pair<pair<int, int>, double**> readCSV(string path){
 
+            int* size = countLines(path);
+            int rows = size[0];
+            int cols = size[1];
 
-                int* size = countLines(path);
-                int rows = size[0];
-                int cols = size[1];
+            delete size;
 
-                delete size;
+            double** data = new double*[rows];
 
-                double** data = new double*[rows];
+            for(int i = 0; i<rows; i++){
+                data[i] = new double[cols];
+            }
 
-                for(int i = 0; i<rows; i++){
-                    data[i] = new double[cols];
-                }
+            ifstream file(path);
 
-                ifstream file(path);
+            if(!file.is_open()){
+                throw string("Unable to open file for reading.");
+            }
 
-                if(!file.is_open()){
-                    throw string("Unable to open file for reading.");
-                }
+            string line;
+            getline(file, line);
+            int i = 0;
+            while(getline(file, line)){
+                stringstream ss(line);
 
+                string val;
+                bool any_null_values = false;
+                int j = 0;
+                while(getline(ss, val, ',')){
 
-                string line;
-                getline(file, line);
-                int i = 0;
-                while(getline(file, line)){
-                    stringstream ss(line);
-
-                    string val;
-                    bool any_null_values = false;
-                    int j = 0;
-                    while(getline(ss, val, ',')){
-
-                        if(val == ""){
-                            any_null_values = true;
-                            break;
-                        }
-                        else{
-                            data[i][j] = stod(val);   
-                        }
-                        j++;
+                    if(val == ""){
+                        any_null_values = true;
+                        break;
                     }
-
-                    if(!any_null_values){
-                        // for(int k = 0; k<9; k++){
-                        //     cout << data[i][k] << ", ";
-                        // }
-                        // cout << endl;
-                        i++;
+                    else{
+                        data[i][j] = stod(val);   
                     }
-
+                    j++;
                 }
 
+                if(!any_null_values){
+                    i++;
+                }
 
-                file.close();
+            }
 
-                return {i, data};
-            
+            file.close();
+
+            return {{i, cols}, data};
         }
 
+};
+
+class LinearRegression {
+
+    private:
+        double* weights;
+        double bias;
+
+        int rows;
+        int cols;
+
+    public:
+
+        LinearRegression(int rows, int cols){
+            this->rows = rows;
+            this->cols = cols;
+            
+            weights = new double[cols];
+            for(int i=0; i<cols; i++){
+                weights[i] = 0;
+            }
+
+            bias = 0;
+        }
+
+        double f(double x[]){
+            double result = 0;
+            
+            for(int i=0; i<cols; i++){
+                result += weights[i] * x[i];
+            }
+            result += bias;
+
+            return result;
+        }
+
+        double* fit(double** data, double learning_rate, int epochs){
+
+            double* cost_history = new double[epochs+1];
+
+            cost_history[0] = compute_cost(data);
+
+            // # pragma omp parallel for
+            for(int i = 0; i<epochs; i++){
+                auto gradients = compute_gradients(data);
+                
+                double* gradients_w = gradients.first;
+                double gradient_b = gradients.second;
+
+                for(int j=0; j<cols; j++){
+                    cout << gradients_w[j] << " ";
+                }
+                cout << endl << gradient_b << endl;
+
+                bias = bias - learning_rate * gradient_b;
+
+                delete gradients_w;
+
+                cost_history[i+1] = compute_cost(data);
+                cout << cost_history[i+1] << endl;
+
+            }
+
+            return cost_history;
+        }
+
+        pair<double*, double> compute_gradients(double** data){
+
+            double* gradients_w = new double[cols];
+            for(int i = 0; i<cols; i++){
+                gradients_w[i] = 0;
+            }
+
+            double gradient_b = 0;
+
+            for(int i = 0; i<rows; i++){
+                double* x = data[i];
+                double y = data[i][cols];
+
+                double difference = f(x) - y;
+
+                for(int j=0; j<cols; j++){
+                    gradients_w[j] += difference * x[j];
+                }
+
+                gradient_b += difference;
+            }
+
+            for(int i = 0; i<cols; i++){
+                gradients_w[i] /= rows;
+            }
+            
+            gradient_b /= rows;
+
+            return {gradients_w, gradient_b};
+        }
+
+        double compute_cost(double** data){
+
+            double loss = 0;
+
+            for(int i = 0; i<rows; i++){
+                loss += pow(f(data[i]) - data[i][cols], 2);
+            }
+            loss /= 2*rows;
+
+            return loss;
+        }
+
+        double predict(double x[]){
+            return f(x);
+        }
+
+        ~LinearRegression(){
+            delete weights;
+        }
 };
 
 int main(){
 
     double** data;
-    int size;
+    int rows, cols;
 
     double start, end;
 
     try{
-        start = omp_get_wtime();
         auto output = Reader::readCSV("./housing.csv");
-        end = omp_get_wtime();
-        cout << end - start << endl;
+        
+        rows = output.first.first;
+        cols = output.first.second;
 
-        size = output.first;
         data = output.second;
     }
     catch(string error){
@@ -214,30 +232,31 @@ int main(){
         return -1;
     }
 
-    return 0;
-
-    int length = sizeof(data)/sizeof(data[0]);
-
-    LinearRegression model(1);
+    LinearRegression model(rows, cols);
 
     start = omp_get_wtime();
 
     int epochs = 100;
-    double* cost_history = model.fit(data, length, 0.01, epochs);
+    double* cost_history = model.fit(data, 0.5, epochs);
     
     end = omp_get_wtime();
 
     printf("Cost History:\n");
     for(int i=0; i<epochs+1; i++){
-        printf("Epoch %d: %.14f\n", i, cost_history[i]);
+        printf("Epoch %d: %f\n", i, cost_history[i]);
     }
 
     delete cost_history;
 
-    double prediction = model.predict(11);
+    double prediction = model.predict(data[8]);
 
-    printf("\nPrediction: %.14f\n", prediction);
+    printf("\nPrediction: %f\n", prediction);
 
     printf("Time: %f\n", end - start);
+
+    for(int i = 0; i<cols; i++){
+        delete data[i];
+    }
+    delete[] data;
 
 }
